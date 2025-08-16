1e7456b25ec52d723ca8e6649c0b3fdc66b97771
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2025-08-16 08:40:09 +0200

    created the stock table

---
+++ Modules/stock.cs
@@ -0,0 +1,22 @@
+using api.Modules; using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Modules
+{
+    public class stock
+    {
+        public int Id { get; set; }
+        public String Symbol { get; set; } = string.Empty;
+        public String CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+        public decimal LastDiv { get; set; }
+        public String Industry { get; set; } = string.Empty;
+        public long MakertCap { get; set; }
+        public List<Comment> comments { get; set; } = new List<Comment>();
+    }
+    
+}

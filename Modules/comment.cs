7380e53f5171709520a633b5f7e3daf0da8a48e1
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2025-08-16 08:44:38 +0200

    created the Comments table for stocks

---
+++ Modules/comment.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Modules
+{
+    public class Comment
+    {
+        public int? StockId { get; set; }
+        public stock? Stock { get; set; }
+        public int Id { get; set; }
+        public String Title { get; set; } = string.Empty;
+        public String Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        
+    }
+}

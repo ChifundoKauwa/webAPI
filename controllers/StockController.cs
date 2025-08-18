67542edaceb16563a9d09ddf92b2ff8799b85e23
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2025-08-18 21:47:12 +0200

    added the DELETE endpoint

--- controllers/StockController.cs
+++ controllers/StockController.cs
@@ -73,6 +73,20 @@ namespace api.controllers
 
         }
 
+        [HttpDelete]
+        [Route("{id}")]
+       public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stock.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+            _context.Stock.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
 
     }
 }

dcb3800221663d376f1034b362a560072b230570
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2025-08-20 04:32:35 +0200

    created the inteface

---
+++ interfaces/IstockInterface.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dto.Stock;
+using api.Modules;
+
+namespace api.interfaces
+{
+    public interface IStockInterface
+    {
+        Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock> CreateAsync(Stock stockModrl);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
+        
+    }
+}

258ddbf70cd52a7e6dc0b0f96f295fc394f4e6f2
ChifundoKauwa <167176163+ChifundoKauwa@users.noreply.github.com>
2025-08-20 04:36:49 +0200

    connected the interface with stockRepo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IStockInterface, StockRepository>();
 
 var app = builder.Build();
 
@@ -23,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
+
 
 
 app.Run();

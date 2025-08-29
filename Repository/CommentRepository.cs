55b1e20272390f0e1037583fa9a8e6395c0cf999
ChifundoKauwa <bsc-com-15-21@unima.ac.mw>
2025-08-29 20:23:18 +0200

    added the delete function

--- Repository/CommentRepository.cs
+++ Repository/CommentRepository.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using api.Data;
 using api.interfaces;
 using api.Modules;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
@@ -18,16 +19,51 @@ namespace api.Repository
             _context = context;
 
         }
-        public async Task<List<Comment>> GetAllAnsyc()
+        public async Task<List<Comment>> GetAllAsync()
         {
             return await _context.Comments.ToListAsync();
         }
 
-        public async Task<Comment?> GetByIdAnsyc(int id)
+        public async Task<Comment?> GetByIdAsync(int id)
         {
-            return await _context.Comments.FindAsync(id); 
+            return await _context.Comments.FindAsync(id);
         }
 
-      
+
+
+        public async Task<Comment> CreateAsync(Comment commentModel)
+        {
+            await _context.Comments.AddAsync(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
+        public async Task<Comment?> UpdateAsync(Comment commentModel, int id)
+        {
+            var existingcomments = await _context.Comments.FindAsync(id);
+            if (existingcomments == null)
+            {
+                return null;
+            }
+            existingcomments.Title = commentModel.Title;
+            existingcomments.Content = commentModel.Content;
+            await _context.SaveChangesAsync();
+            return existingcomments;
+        }
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var commentModel = await _context.Comments.FirstOrDefaultAsync(c => c.Id == id);
+            if (commentModel == null)
+            {
+                return null;
+            }
+            _context.Comments.Remove(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
     }
+
+       
 }
